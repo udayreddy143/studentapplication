5811922e241597f5f4d67a78595c4c231d30df6f
Dastagiri <121944040+Dastagiri-s@users.noreply.github.com>
2025-08-23 22:00:29 +0530

    added Controller repository layer

---
+++ StudentApplication/StudentApplication/src/main/java/com/jswin/StudentApplication/controller/StudentController.java
@@ -0,0 +1,23 @@
+package com.jswin.StudentApplication.controller;
+
+
+import com.jswin.StudentApplication.dto.Student;
+import com.jswin.StudentApplication.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+
+    @PostMapping("/save")
+    public void saveStudent(@RequestBody Student student)
+    {
+        studentService.saveStudentDetails(student);
+    }
+}
