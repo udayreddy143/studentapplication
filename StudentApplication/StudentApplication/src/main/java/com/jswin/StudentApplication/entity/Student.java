129a91d286e79508d3f5bd03bccc1899a9783779
Dastagiri <121944040+Dastagiri-s@users.noreply.github.com>
2025-08-23 21:23:33 +0530

    added jpa repository layer

---
+++ StudentApplication/StudentApplication/src/main/java/com/jswin/StudentApplication/entity/Student.java
@@ -0,0 +1,12 @@
+package com.jswin.StudentApplication.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Student {
+
+
+    private Long id;
+    private String name;
+    private String email;
+}
--- StudentApplication/StudentApplication/src/main/java/com/jswin/StudentApplication/repo/StudentRepo.java
+++ StudentApplication/StudentApplication/src/main/java/com/jswin/StudentApplication/repo/StudentRepo.java
@@ -1,7 +1,11 @@
 package com.jswin.StudentApplication.repo;
 
+import com.jswin.StudentApplication.service.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import com.jswin.StudentApplication.entity.Student;
 @Component
-public interface StudentRepo {
+public interface StudentRepo extends JpaRepository<Student,Long> {
+
 }
